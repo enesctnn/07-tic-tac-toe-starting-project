926a7b047aaca3439c9ebc14e82681302a3f5f0a
enesctnn <enespcetin@gmail.com>
2023-10-23 23:25:18 +0300

    player name editing buttons interactive

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,30 @@
+import { useRef, useState } from 'react';
+import classes from './Player.module.css';
+
 const Player = ({ name, symbol }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(name);
+  const inputRef = useRef();
+
+  const buttonClickHandler = () => {
+    setIsEditing((prevStat) => {
+      return !prevStat;
+    });
+    if (isEditing) {
+      setPlayerName(inputRef.current.value);
+    }
+  };
   return (
     <li>
-      <span className="player-info">
-        <span className="player-name">{name}</span>
-        <span className="player-symbol">{symbol}</span>
+      <span className={classes['player-info']}>
+        {!isEditing && (
+          <span className={classes['player-name']}>{playerName}</span>
+        )}
+        {isEditing && <input type="text" ref={inputRef} />}
+        <span className={classes['player-symbol']}>{symbol}</span>
       </span>
-      <button>Edit</button>
+      {!isEditing && <button onClick={buttonClickHandler}>Edit</button>}
+      {isEditing && <button onClick={buttonClickHandler}>Save</button>}
     </li>
   );
 };
